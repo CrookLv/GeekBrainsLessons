21cc2a29cc51fe525d1b7905e2cbffa49dc0b3b2
CrookLv <86350349+CrookLv@users.noreply.github.com>
2021-08-02 14:08:24 +0800

    tier3-02
    homework

---
+++ tier3-02.py
@@ -0,0 +1,24 @@
+# author - Vyacheslav Gusev
+
+number = int(input("Введите номер месяца: "))
+if number <= 12 and number >= 1:
+  month_dict = {1: 'Январь',
+                2: 'Февраль',
+                3: 'Март',
+                4: 'Апрель',
+                5: 'Май',
+                6: 'Июнь',
+                7: 'Июль',
+                8: 'Август',
+                9: 'Сентябрь',
+                10: 'Октябрь',
+                11: 'Ноябрь',
+                12: 'Декабрь'}
+  month_list = list(month_dict.values())
+    for i, el in enumerate(month_list):
+      if i == number-1:
+        print(f"Месяц из list это {month_list[i]}")
+        break
+    print(f"Месяц из dict это {month_dict[number]}")
+else:
+    print("Ошибка")
